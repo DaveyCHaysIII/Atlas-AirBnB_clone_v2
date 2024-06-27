6472b03e6f6c546837492015975140d68afb1467
3lackrukh <nathan.rhys@atlasschool.com>
2024-06-27 22:31:31 +0000

    created db_storage.py. imports os, create_engine, sessionmaker and  all models

---
+++ models/engine/db_storage.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""This module defines a class to manage database storage for hbnb clone"""
+import os
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, scoped_session
+from models.base_model import BaseModel, Base
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
+
+
+class DBStorage:
+    """Manages storage of HBNB models in a MySQL database"""
+    __engine = None
+    __session = None
