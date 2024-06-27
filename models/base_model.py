ee2fcfa4dac7ba49f48bbb460908e1871c09ddcd
3lackrukh <nathan.rhys@atlasschool.com>
2024-06-27 23:38:40 +0000

    Corrected comments style lines 21, 24, 27

--- models/base_model.py
+++ models/base_model.py
@@ -18,13 +18,13 @@ class BaseModel:
         else:
             for k, v in kwargs.items():
                 if k == 'created_at' or k == 'updated_at':
-                    #convert string values to formatted datetime objects
+                    # convert string values to formatted datetime objects
                     v = datetime.strptime(v, time_format)
                 if k != '__class__':
-                    #set all attributes except for class
+                    # set all attributes except for class
                     setattr(self, k, v)
                 if "id" not in kwargs:
-                    #supply id and timestamps for new instances from kwargs
+                    # supply id and timestamps for new instances from kwargs
                     self.id = str(uuid.uuid4())
                     self.created_at = datetime.now()
                     self.updated_at = datetime.now()
