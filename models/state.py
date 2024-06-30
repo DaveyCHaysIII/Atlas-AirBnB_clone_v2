f99d0956cb19b661b1e15f8a9a0369c29f2196d6
3lackrukh <nathan.rhys@atlasschool.com>
2024-06-30 03:35:57 +0000

    updates State class with attributes dependent on storage type. Includes backref relationship to city, deletes dependent instances when state is deleted

--- models/state.py
+++ models/state.py
@@ -1,8 +1,27 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models import storage, storage_t
+from models.base_model import BaseModel, Base
+from models.city import City
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 
-
-class State(BaseModel):
+class State(BaseModel, Base):
     """ State class """
-    name = ""
+    if storage_t == 'db':
+        __tablename__ = 'states'
+        name = Column(String(128), nullable=False)
+        cities = relationship("City", backref='state', cascade="all, delete-orphan")
+
+    else:
+        name = ''
+
+        @property
+        def cities(self):
+            """ getter for cities"""
+            city_list = []
+            all_cities = storage.all(City)
+            for city in all_cities.values():
+                if city.state_id == self.id:
+                    city_list.append(city)
+            return city_list
