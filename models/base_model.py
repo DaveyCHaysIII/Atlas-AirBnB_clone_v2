0b879317d1d66e7b67852ba06241a226389327d2
3lackrukh <nathan.rhys@atlasschool.com>
2024-06-30 02:17:57 +0000

    adds a global variable storage_t determines storage type for base_model functionality.
    changes datetime to utc

--- models/base_model.py
+++ models/base_model.py
@@ -1,27 +1,43 @@
 #!/usr/bin/python3
 """This module defines a base class for all models in our hbnb clone"""
 import models
+from models import storage
+from models import storage_t
 import uuid
-from datetime import datetime
+from datetime import datetime, timezone
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, DateTime, String
 
-Base = declarative_base()
+if storage_t == 'db':
+    Base = declarative_base()
+else:
+    Base = object
 
 
 class BaseModel:
     """A base class for all hbnb models"""
-    id = Column(String(60), nullable=False, primary_key=True, unique=True)
-    created_at = Column(DateTime, nullable=False, default=datetime.utcnow())
-    updated_at = Column(DateTime, nullable=False, default=datetime.utcnow())
+    if storage_t == 'db':
+        id = Column(
+            String(60),
+            nullable=False,
+            primary_key=True,
+            unique=True)
+        created_at = Column(
+            DateTime(timezone=True),
+            nullable=False,
+            default=datetime.now(timezone.utc))
+        updated_at = Column(
+            DateTime(timezone=True),
+            nullable=False,
+            default=datetime.now(timezone.utc))
 
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
-        time_format = "%Y-%m-%dT%H:%M:%S.%f"
+        time_format = '%Y-%m-%dT%H:%M:%S.%f'
         if not kwargs:
             self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
+            self.created_at = datetime.now(timezone.utc)
+            self.updated_at = datetime.now(timezone.utc)
 
         else:
             for k, v in kwargs.items():
@@ -31,29 +47,26 @@ class BaseModel:
                 if k != '__class__':
                     # set all attributes except for class
                     setattr(self, k, v)
-                if "id" not in kwargs:
-                    # supply id and timestamps for new instances from kwargs
+                if 'id' not in kwargs:
+                    # supply id if missing
                     self.id = str(uuid.uuid4())
-                    self.created_at = datetime.now()
-                    self.updated_at = datetime.now()
 
     def __str__(self):
         """Returns a string representation of the instance"""
-        return "[{}] ({}) {}".format(
+        return '[{}] ({}) {}'.format(
             self.__class__.__name__,
             self.id,
             self.__dict__)
 
     def save(self):
         """Updates updated_at with current time when instance is changed"""
-        self.updated_at = datetime.now()
+        self.updated_at = datetime.now(timezone.utc)
         models.storage.new(self)
         models.storage.save()
 
     def to_dict(self):
         """Convert instance into dict format"""
-        dictionary = {}
-        dictionary.update(self.__dict__)
+        dictionary = self.__dict__.copy()
         dictionary["__class__"] = self.__class__.__name__
         dictionary['created_at'] = self.created_at.isoformat()
         dictionary['updated_at'] = self.updated_at.isoformat()
@@ -63,5 +76,4 @@ class BaseModel:
 
     def delete(self):
         """delete the current instance from the storage"""
-        from models import storage
         storage.delete(self)
