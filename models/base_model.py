2e8f874f89e868f0afb834e3c513730be0bb2d44
3lackrukh <nathan.rhys@atlasschool.com>
2024-07-01 17:52:58 +0000

    added timezone formatting to time_format line 34

--- models/base_model.py
+++ models/base_model.py
@@ -31,7 +31,7 @@ class BaseModel:
 
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
-        time_format = '%Y-%m-%dT%H:%M:%S.%f'
+        time_format = '%Y-%m-%dT%H:%M:%S.%f%z'
         if not kwargs:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now(timezone.utc)
