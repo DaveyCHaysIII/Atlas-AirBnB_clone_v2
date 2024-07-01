eaf9068aa53c6fbc4c23a72e90f746ed4a97e743
3lackrukh <nathan.rhys@atlasschool.com>
2024-07-01 17:20:16 +0000

    pycode styling

--- models/place.py
+++ models/place.py
@@ -12,20 +12,20 @@ class Place(BaseModel, Base):
     if models.storage_t == 'db':
         __tablename__ = 'places'
         place_amenity = Table('place_amenity',
-                      Base.metadata,
-                      Column(
-                          'place_id',
-                          String(60),
-                          ForeignKey('places.id'),
-                          primary_key=True,
-                          nullable=False
-                          ),
-                      Column(
-                          'amenity_id',
-                          String(60),
-                          ForeignKey('amenities.id'),
-                          primary_key=True,
-                          nullable=False))
+                    Base.metadata,
+                    Column(
+                        'place_id',
+                        String(60),
+                        ForeignKey('places.id'),
+                        primary_key=True,
+                        nullable=False
+                        ),
+                    Column(
+                        'amenity_id',
+                        String(60),
+                        ForeignKey('amenities.id'),
+                        primary_key=True,
+                        nullable=False))
         amenities = relationship(
             "Amenity",
             secondary=place_amenity,
