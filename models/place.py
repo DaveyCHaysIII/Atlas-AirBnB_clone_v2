111ae2cab75ac2395ee623ffbf35ef95622d409b
3lackrukh <nathan.rhys@atlasschool.com>
2024-07-01 17:31:59 +0000

    indent fixes lines 14-28

--- models/place.py
+++ models/place.py
@@ -11,79 +11,67 @@ class Place(BaseModel, Base):
     """ A place to stay """
     if storage_t == 'db':
         __tablename__ = 'places'
-        place_amenity = Table('place_amenity',
-                    Base.metadata,
-                    Column(
-                        'place_id',
-                        String(60),
-                        ForeignKey('places.id'),
-                        primary_key=True,
-                        nullable=False
-                        ),
-                    Column(
-                        'amenity_id',
-                        String(60),
-                        ForeignKey('amenities.id'),
-                        primary_key=True,
-                        nullable=False))
+        place_amenity = Table(
+            'place_amenity',
+            Base.metadata,
+            Column(
+                'place_id',
+                String(60),
+                ForeignKey('places.id'),
+                primary_key=True,
+                nullable=False),
+            Column(
+                'amenity_id',
+                String(60),
+                ForeignKey('amenities.id'),
+                primary_key=True,
+                nullable=False))
         amenities = relationship(
             "Amenity",
             secondary=place_amenity,
-            back_populates="place_amenities"
-            )
+            back_populates="place_amenities")
         reviews = relationship(
             "Review",
             backref='place',
-            cascade="all, delete-orphan"
-            )
+            cascade="all, delete-orphan")
         city_id = Column(
             String(60),
             ForeignKey('cities.id'),
-            nullable=False
-        )
+            nullable=False)
         user_id = Column(
             String(60),
             ForeignKey('users.id'),
-            nullable=False
-        )
+            nullable=False)
         name = Column(
             String(128),
-            nullable=False
-        )
+            nullable=False)
         description = Column(
             String(1024),
-            nullable=True
-        )
+            nullable=True)
         number_rooms = Column(
             Integer,
             default=0,
-            nullable=False
-        )
+            nullable=False)
         number_bathrooms = Column(
             Integer,
             default=0,
-            nullable=False
-        )
+            nullable=False)
         max_guest = Column(
             Integer,
             default=0,
-            nullable=False
-        )
+            nullable=False)
         price_by_night = Column(
             Integer,
             default=0,
-            nullable=False
-        )
+            nullable=False)
         latitude = Column(
             Float,
             default=0,
-            nullable=True
-        )
+            nullable=True)
         longitude = Column(
             Float,
             default=0,
-            nullable=True
-        )
+            nullable=True)
 
     else:
         city_id = ""
