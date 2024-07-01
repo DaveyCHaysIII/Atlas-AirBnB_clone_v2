4d5f3d633e22831e15359c451f46a67c6e98b217
3lackrukh <nathan.rhys@atlasschool.com>
2024-07-01 17:47:43 +0000

    attempting to correct circular import. moves import of storage_t into class definition

--- models/user.py
+++ models/user.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 """This module defines a class User"""
 from sqlalchemy import Column, String
-import models
 from models.base_model import BaseModel, Base
 from sqlalchemy.orm import relationship
 
 
 class User(BaseModel, Base):
     """This class defines a user by various attributes"""
-    if models.storage_t == 'db':
+    from models import storage_t
+    if storage_t == 'db':
         __tablename__ = 'users'
         places = relationship(
             "Place",
