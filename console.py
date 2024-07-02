be6cf83e6afcb93e898601ae17f9ced26a9d18f7
DaveyCHaysIII <david.c.hays.iii@gmail.com>
2024-07-02 02:36:21 +0000

    useless commit

--- console.py
+++ console.py
@@ -260,7 +260,7 @@ class HBNBCommand(cmd.Cmd):
         print("Shows all objects, or all of a class")
         print("[Usage]: all <className>\n")
 
-    def do_count(self, args):
+    def do_count(self, args): 
         """Count current number of class instances"""
         count = 0
         for k, v in storage._FileStorage__objects.items():
