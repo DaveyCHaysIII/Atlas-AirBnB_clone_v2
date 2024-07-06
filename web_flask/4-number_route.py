d061df9e89837dbb21c8dac4b3f8945d5e3ea07a
DaveyCHaysIII <david.c.hays.iii@gmail.com>
2024-07-06 15:49:36 +0000

    pushing what I have before changes to docker container

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+"""Write a script that starts a Flask web application
+three routes, / , /hbnb, and /c/<text>"""
+from flask import Flask
+from markupsafe import escape
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello_world():
+    """adds the / route"""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hello_world_hbnb():
+    """adds the /hbnb route"""
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def hello_world_c(text):
+    """adds the /c/<text> route, with sanitization"""
+    formatted_text = escape(text).replace('_', ' ')
+    return f"C {formatted_text}"
+
+
+@app.route("/python/", strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def hello_world_Python(text='is cool'):
+    """adds the /Python/<text> route, with sanitization"""
+    formatted_text = escape(text).replace('_', ' ')
+    return f"Python {formatted_text}"
+
+
+@app.route("/number/<n>", strict_slashes=False)
+def hello_world_n(n):
+    """adds the /number/<n> route, with sanitization"""
+    if isinstance(n, int):
+        return f"{n} is a number"
+
+
+if __name__ == "__main__": 
+    app.run(host="0.0.0.0", port=5000)
